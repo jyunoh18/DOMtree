ff7d040646aed7d81adf62369cc4294b400248b3
jyunoh18 <30875152+jyunoh18@users.noreply.github.com>
2017-09-19 08:18:22 -1000

    Create innerhtml.js

---
+++ innerhtml.js
@@ -0,0 +1,13 @@
+<span id = "name"></span>
+<span>went to the store and</span>
+
+<span id="verb"></span>
+<span>a rock</span>
+
+------------------------------------------
+
+var x = prompt("Enter a name");
+var y = prompt("Enter a verb");
+
+document.getElementById('name').innerHTML = x
+document.getElementById('verb').innerHTML = y
