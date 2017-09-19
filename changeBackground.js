84c440e74f999cea913183865d299f1c2ae5df32
jyunoh18 <30875152+jyunoh18@users.noreply.github.com>
2017-09-19 08:59:11 -1000

    Create changeBackground.js

---
+++ changeBackground.js
@@ -0,0 +1,11 @@
+<button onclick="changeColor()">Change Background</button>
+
+----------------------------------------------------------------
+
+function changeColor() {
+
+document.body.style.background =
+	'url("https://assets.pcmag.com/media/images/396774-the-10-best-ultraportables.jpg")';
+	document.body.style.backgroundSize = 'cover';
+
+}
